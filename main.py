8b9a2779f1c4ea421ec9d64e9ac08c979104bc22
akaashpreetham <akaash.preethu99@gmail.com>
2020-01-03 13:23:40 +0530

    Added input function to get resolutions

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-import os
-import pandas as pd 
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  3 13:09:38 2020
 
+@author: Akaash Preetham
+"""
+
+import pandas as pd
+import os
+import numpy as np 
+#import pandas as pd 
+
+# FUNCTIONALITIES
+
+# Input resolution
+name = input("Enter the name: ")
+goal = input("Enter the goal: ")
+unit = input("Enter the unit: ")
+resolution = []
+resolution.append(name)
+resolution.append(goal)
+resolution.append(unit)
+print(resolution)
+resolutions.append(resolution)
+
+df = pd.DataFrame(resolutions, columns = ['Name', 'Goal', 'Unit'])
+df.to_csv('Resolutions.xlsx')
+
+# Display resolution 
+pd.read_csv('Resolutions.xlsx')
+
+# Update performance
+
+# Reminder
